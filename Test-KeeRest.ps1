34f6ba252d90a95f7fb0025cc43e6db452444fbf
eladent <eladent@gmail.com>
2019-11-21 21:06:11 +0100

    Updated Tests using new variables name

--- Test-KeeRest.ps1
+++ Test-KeeRest.ps1
@@ -3,17 +3,16 @@ Import-Module $PSScriptRoot\KeeRest.psm1 -Force
 # https://devblogs.microsoft.com/scripting/testing-script-modules-with-pester/
 
 Describe 'Connect-KeepassDB' {
-
-    Context 'Connects' {
-        $DBObj = Connect-KeepassDB -DbPath ".\Tests\TestDatabase.kdbx" -Password (ConvertTo-SecureString "KeeRest_Rocks!" -AsPlainText -Force)
-
+    $DBObj = Connect-KeepassDB -KDBXDatabasePath "$PSScriptRoot\Tests\TestDatabase.kdbx" -KDBXPassword (ConvertTo-SecureString "KeeRest_Rocks!" -AsPlainText -Force)   
+    Context 'Connects' {         
         it 'connects to the DB' {
             $DBObj.GetType().Name | should be "PwDatabase"
             $DBObj.IsOpen | should be $True
         }
     }
+    
+    Lock-KeepassDB -KDBXDatabase $DBObj
     Context 'Disconnects' {
-        Lock-KeepassDB -Database $DBObj
         it 'Disconnects from the DB' {
             $DBObj.IsOpen | should be $false
         }
@@ -21,16 +20,15 @@ Describe 'Connect-KeepassDB' {
  }
 
  Describe 'Get-KeepassEntry' {
-
     Context 'Communicates with the KDBX' {
-        $DBObj = Connect-KeepassDB -DbPath ".\Tests\TestDatabase.kdbx" -Password (ConvertTo-SecureString "KeeRest_Rocks!" -AsPlainText -Force)
-        $info = Get-KeepassEntry -Database $DBObj -EntryTitleFilter "Arnaud"
+        $DBObj = Connect-KeepassDB -KDBXDatabasePath "$PSScriptRoot\Tests\TestDatabase.kdbx" -KDBXPassword (ConvertTo-SecureString "KeeRest_Rocks!" -AsPlainText -Force)
+        $info = Get-KeepassEntry -KDBXDatabase $DBObj -ValueFilter "Arnaud"
         it 'Get and entry informations correctly' {
             $info.Title | should BeExactly "Arnaud"
             $info.Password  | should BeExactly "IB6WkecQ2wWm9VaLgksq"
             $info.UserName  | should BeExactly "apetitjean"
         }
-        $info = Get-KeepassEntry -Database $DBObj -EntryTitleFilter "^Ar"
+        $info = Get-KeepassEntry -KDBXDatabase $DBObj -FieldFilter 'Title' -ValueFilter "^Ar"
         it 'returns multiple entries correctly' {
             $info.Length  | should Be 2
         }
