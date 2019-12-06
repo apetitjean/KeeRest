31c30f051647684ddb778f30d4b2298f82d2c58c
eladent <etienne@ladent.fr>
2019-12-06 17:24:13 +0100

    Add Test for New & Remove -KeepassEntry

--- Test-KeeRest.ps1
+++ Test-KeeRest.ps1
@@ -17,9 +17,9 @@ Describe 'Connect-KeepassDB' {
             $DBObj.IsOpen | should be $false
         }
     }
- }
+}
 
- Describe 'Get-KeepassEntry' {
+Describe 'Get-KeepassEntry' {
     Context 'Communicates with the KDBX' {
         $DBObj = Connect-KeepassDB -KDBXDatabasePath "$PSScriptRoot\Tests\TestDatabase.kdbx" -KDBXPassword (ConvertTo-SecureString "KeeRest_Rocks!" -AsPlainText -Force)
         $info = Get-KeepassEntry -KDBXDatabase $DBObj -ValueFilter "Arnaud"
@@ -32,5 +32,39 @@ Describe 'Connect-KeepassDB' {
         it 'returns multiple entries correctly' {
             $info.Length  | should Be 2
         }
-     }
- }
+    }
+}
+
+Describe 'New-KeepassEntry and Remove-KeepassEntry' {
+    Context 'Creates new entry in the KDBX' {
+        $DBObj = Connect-KeepassDB -KDBXDatabasePath "$PSScriptRoot\Tests\TestDatabase.kdbx" -KDBXPassword (ConvertTo-SecureString "KeeRest_Rocks!" -AsPlainText -Force)
+        $NewEntryInfos = @{
+            Title = "TesTPesterEntry"
+            UserName = "Pester"
+            Password = "Pester-T3sts_Rocks!"
+            URL = 'https://github.com/apetitjean/KeeRest'
+            Notes = "Thumb up if you found this searching GitHub for Passw0rd!"
+            Tags = "Test, Pester, Keerest"
+        }
+        New-KeepassEntry -KDBXDatabase $DBObj -EntryInfos $NewEntryInfos
+        $createdEntry = Get-KeepassEntry -KDBXDatabase $DBObj -FieldFilter 'UserName' -ValueFilter "Pester"
+        Lock-KeepassDB -KDBXDatabase $DBObj
+        $DBObj = Connect-KeepassDB -KDBXDatabasePath "$PSScriptRoot\Tests\TestDatabase.kdbx" -KDBXPassword (ConvertTo-SecureString "KeeRest_Rocks!" -AsPlainText -Force)
+        it 'Create a new entry correctly' {
+            $createdEntry.Title | Should BeExactly $NewEntryInfos.Title
+            $createdEntry.UserName | Should BeExactly $NewEntryInfos.UserName
+            $createdEntry.Password | Should BeExactly $NewEntryInfos.Password
+            $createdEntry.URL  | Should BeExactly $NewEntryInfos.URL
+            $createdEntry.Note  | Should BeExactly $NewEntryInfos.Note
+            #TODO tags
+        }
+        $Uuid = $createdEntry.Uuid
+        Remove-KeeRestEntry -KDBXDatabase $DBObj -EntryUuid $Uuid
+        Lock-KeepassDB -KDBXDatabase $DBObj
+        $DBObj = Connect-KeepassDB -KDBXDatabasePath "$PSScriptRoot\Tests\TestDatabase.kdbx" -KDBXPassword (ConvertTo-SecureString "KeeRest_Rocks!" -AsPlainText -Force)
+        $DeletedEntry = Get-KeepassEntry -KDBXDatabase $DBObj -FieldFilter 'UserName' -ValueFilter "Pester"
+        it 'returns multiple entries correctly' {
+            $DeletedEntry | should Be $null
+        }
+    }
+}
